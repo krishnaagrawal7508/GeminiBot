2512fb22b230713a26b3c1b4c9d5ab153dd1beb8
krishna agrawal <agrawalkrishna522@gmail.com>
2025-07-01 16:32:52 +0530

    react build config fixed

--- src/chat-app/vite.config.js
+++ src/chat-app/vite.config.js
@@ -6,7 +6,20 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
-    outDir: path.resolve(__dirname, '../views/chatPanel/dist'), // Output here
+    outDir: path.resolve(__dirname, '../views/chatPanel/dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'index.css';
+          }
+          return '[name][extname]';
+        }
+      }
+    },
+    filename: 'index.html'
   },
 });
